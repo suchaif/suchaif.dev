481d015fb4866ed9faf3375e94bfac0923f33801
suchaif <suchai.f@gmail.com>
2020-05-16 00:54:56 +0700

    Remove markdown-it-task-lists

--- blogs/.vuepress/config.js
+++ blogs/.vuepress/config.js
@@ -1,9 +1,4 @@
 module.exports = {
   title: 'Suchai.f - be a better version of yourself :)',
   description: 'ahhh just start this website guys, please be patient.',
-  markdown: {
-    extendMarkdown: md => {
-      md.use(require('markdown-it-task-lists'))
-    }
-  }
 }
